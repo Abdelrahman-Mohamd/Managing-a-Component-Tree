229f5a1596affea73a6bc288dd26c6fb96df47bb
Abdelrahman Mohamed <88936527+Abdelrahman-Mohamd@users.noreply.github.com>
2025-01-27 14:20:59 +0200

    Initial commit

---
+++ src/components/ToDoItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ToDoItem(props) {
+  return (
+    <li
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      {props.text}
+    </li>
+  );
+}
+export default ToDoItem;
